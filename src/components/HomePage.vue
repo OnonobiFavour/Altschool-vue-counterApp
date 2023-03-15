75c2f8fea9f49f162fecc22e59292fe19b14c26e
unknown <phayvorononobi@gmail.com>
2023-03-15 09:11:20 -1100

    commit

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -12,10 +12,10 @@
             <section class="home_page">
                 <div class="text">
                     <h1>A Simple Counter App </h1>
-                    <p>Made with vuejs</p>
+                    <p class="p">Made with vuejs</p>
                 </div>
                 <router-link to="/counter" class="a">start</router-link>
-                <!-- <button class="__btn">start</button> -->
+                
             </section>
         </section>
         
@@ -25,18 +25,26 @@
     <section class="features">
         <h2>Features</h2>
         <div class="text__features">
-            <h4>This counter App was created for the purpose of making counts with the use of increment,decrement, reset and setValue functions. The app is of great UI and was created with Vue js. <br>Project by Ononobi Favour✨..
-            <br><button class="github">
-                <a href="https://github.com/OnonobiFavour">GitHub repo</a>
-            </button>
-            </h4>
+            <p>This counter App was created for the purpose of making counts with the use of increment,decrement, reset and setValue functions. The app is of great UI and was created with Vue js. <br>Project by Ononobi Favour✨..
+            </p>
+            <div class="btn_github">
+                
+                <button class="github">
+                    <a href="https://github.com/OnonobiFavour">GitHub repo</a>
+                </button>
+            </div>
         </div>
         
     </section>
 </template>
 
 <style scoped>
-    
+.header{
+    width: 100%;
+    height: 100%;
+    clip-path: ellipse(100% 55% at 44% 37%);
+    background: linear-gradient(to right,rgb(228, 70, 97), rgb(193, 46, 75) ); 
+}    
 nav {
   display: flex;
   justify-content: space-between;
@@ -51,6 +59,7 @@ nav ul {
 
 nav ul li {
   margin: 0 1rem;
+  font-family: 'Courier New', Courier, monospace;
 }
 
 nav ul li a {
@@ -70,12 +79,7 @@ nav ul li a:hover {
     width: 100%;
     height: 100%;        
 }
-.header{
-    width: 100vw;
-    height: 100%;
-    clip-path: ellipse(100% 55% at 44% 37%);
-    background: linear-gradient(to right,rgb(228, 70, 97), rgb(193, 46, 75) ); 
-}
+
 .home_page{
     display: flex;
     flex-direction: column;
@@ -89,22 +93,15 @@ nav ul li a:hover {
     justify-content: center;
     
 }
-@keyframes home-textBox{
-    from{
-        opacity: 0.5;
-        transform: translateY(100%);
-    }
-    to{
-        opacity: 1;
-        transform: translateY(0%);
-    }
-}
+
 h1{
     font-size: 40px;
+    font-family: Arial, Helvetica, sans-serif;
     letter-spacing: 0.2rem;
 }
-p{
+.p{
     font-size: 20px;
+    font-family: bahnschrift;
     padding: 1rem 0rem;
 }
 .text{
@@ -116,38 +113,56 @@ p{
     text-decoration: none;
     font-weight: bolder;
     padding-top: 8px;
-    width: 150px;
+    width: 250px;
     text-align: center;
     height: 40px;
     font-size: 20px;
     background: inherit;
     border: 1px solid white;
-    border-radius: 10px;
+    border-radius: 20px;
 }
 .a:hover{
     color: rgb(228, 70, 97);
     background-color: #fff;
     transition: 0.4s;
 }
+@keyframes home-textBox{
+    from{
+        opacity: 0.5;
+        transform: translateY(100%);
+    }
+    to{
+        opacity: 1;
+        transform: translateY(0%);
+    }
+}
 .features{
-    position: absolute;
-    max-width: 1000px;
-    left: 7%;
-    transform: translate(-50% 50%);
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
 }
 .text__features{
     font-size: 20px;
-    max-width: 100%;
-    height: 200px;
-    padding: 1rem;
+    max-width: 550px;
+    padding: 0.6rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    flex-direction: column;
     color: grey;
 }
 h2{
     font-size: 40px;
+    font-family: bahnschrift;
     text-align: center;
 }
-h4{
+.text_feature p{
     text-align: center;
+    /* width: 300px; */
+    background: red;
 }
 .github{
     background-color: rgb(228, 70, 97);
